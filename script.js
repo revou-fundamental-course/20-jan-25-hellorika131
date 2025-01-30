f0fe8b5a2329a89638d2203bcb816c13184d8997
rika <sitikoniatulf01@gmail.com>
2025-01-30 15:35:35 +0700

    update index css and js

--- script.js
+++ script.js
@@ -1,18 +1,46 @@
 const bmiText = document.getElementById('bmi');
 const descText = document.getElementById('desc');
-const form = document.getElementsByName('form');
+const form = document.querySelector('form'); 
 
-form.addEventListener('submit', onFormSubmit)
+form.addEventListener('submit', onFormSubmit);
+form.addEventListener('reset', onFormReset);
+
+function onFormReset() {
+    bmiText.textContent = 0;
+    bmiText.className = '';
+    descText.textContent = "N/A";
+}
 
 function onFormSubmit(e) {
     e.preventDefault();
 
-    const weight = parseFloat(form.weight.value);
-    const height = parseFloat(form.height.value);
+    const weight = parseFloat(document.getElementById('weight').value);
+    const height = parseFloat(document.getElementById('height').value);
 
     if (isNaN(weight) || isNaN(height) || weight <= 0 || height <= 0) {
-    alert('Masukkan berat dan tinggi badan yang benar');
-    return; 
-}
+        alert('Masukkan berat dan tinggi badan yang benar');
+        return; 
+    }
+
+    const heightInMeters = height / 100;
+    const bmi = weight / Math.pow(heightInMeters, 2);
+
+    bmiText.textContent = bmi.toFixed(2);
+
+    let desc = "";
+    if (bmi < 18.5) {
+        desc = "Kurang";
+    } else if (bmi >= 18.5 && bmi < 25) {
+        desc = "Normal";
+    } else if (bmi >= 25 && bmi < 30) {
+        desc = "Berlebih";
+    } else {
+        desc = "Obesitas";
+    }
+
+    bmiText.classList.remove("Kurang", "Normal", "Berlebih", "Obesitas");
+    bmiText.classList.add(desc);
+
+    descText.innerHTML = `Hasil BMI anda <strong>${desc}</strong>`;
 
 }
